b0847df4f38715a8a40ea06c22ad136ab65e6ab7
anarodriguezm <146813513+anarodriguezm@users.noreply.github.com>
2024-02-17 11:23:44 +0100

    Delete scripts/1.3.1.env.sh

--- scripts/1.3.1.env.sh
+++
@@ -1,8 +0,0 @@
-# Contraseña para el usuario root de MariaDB
-MARIADB_ROOT_PASSWORD=anaydavid
-
-# Nombre de usuario de MariaDB
-MARIADB_USER=anaydavid
-
-# Contraseña para PhpMyAdmin
-PHPMYADMIN_PASSWORD=anaydavid
